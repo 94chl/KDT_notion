84e3ff22cc278785473772c1b6e139903fc8ba96
Yongrok <49222172+94chl@users.noreply.github.com>
2021-09-04 13:38:15 +0900

    Update App.js

--- src/component/App.js
+++ src/component/App.js
@@ -50,8 +50,8 @@ export default function App({ $target, mainPageId, refreshList }) {
     mainPageId = getItem("mainPageId", false);
 
     const { pathname } = window.location;
+    if (pathname === "/KDTRK/") {
     console.log(pathname)
-    if (pathname === "/KDTRK") {
       if (mainPageId != "initialPage") {
         const mainPageInfo = (
           await request(`/documents`, { method: "GET" })
@@ -60,6 +60,7 @@ export default function App({ $target, mainPageId, refreshList }) {
         postsPage.setState(mainPageInfo);
       }
     } else if (pathname.indexOf("/documents/") === 0) {
+    console.log(pathname)
       const [, , postId] = pathname.split("/");
       let getPost = { title: "", content: defaultContent };
       if (btnType !== "new") {
