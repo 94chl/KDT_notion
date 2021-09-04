f21e05e89221855947baa7ed144231b29afc8316
Yongrok <49222172+94chl@users.noreply.github.com>
2021-09-04 10:14:10 +0900

    Update App.js

--- src/component/App.js
+++ src/component/App.js
@@ -4,21 +4,22 @@ import { initRouter } from "../router.js";
 import { parseRes, request } from "../api.js";
 import { getItem } from "../storage.js";
 
+const defaultContent = new Map([
+  ["Content", ""],
+  ["Comment", ""],
+  ["Assign", ""],
+  ["State", ""],
+  ["Property", ""],
+  ["Updated", new Date()],
+  ["Due Date", ""],
+]);
+
 export default function App({ $target, mainPageId, refreshList }) {
   const postsPage = new PostsPage({
     $target,
     mainPageId,
   });
-
-  const defaultContent = new Map([
-    ["Content", ""],
-    ["Comment", ""],
-    ["Assign", ""],
-    ["State", ""],
-    ["Property", ""],
-    ["Updated", new Date()],
-    ["Due Date", ""],
-  ]);
+  console.log("post page"+mainPageId)
 
   const postEditPage = new PostEditPage({
     $target,
@@ -32,6 +33,7 @@ export default function App({ $target, mainPageId, refreshList }) {
       refreshList();
     },
   });
+  console.log("post edit page"+mainPageId)
 
   this.route = async (btnType, id, parentId) => {
     if (btnType === "close") {
