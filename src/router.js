0cf5513959c411c519d84a46ee7eca2d4dfaccd2
Yongrok <49222172+94chl@users.noreply.github.com>
2021-09-13 18:22:23 +0900

    Update router.js

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ export const initRouter = (onRoute) => {
     const { nextUrl, btnType, id, parentId } = e.detail;
 
     if (nextUrl) {
-      history.pushState(null, null, `/KDTRK${nextUrl}`);
+      history.pushState(null, null, `/KDT_notion{nextUrl}`);
       onRoute(btnType, id, parentId);
     } else {
       console.log("no url!");
